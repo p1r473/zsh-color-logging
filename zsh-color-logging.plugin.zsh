1e24ed20b1075a7ac6259bd98f12bacdef5aff03
p1r473 <subwayjared@gmail.com>
2024-05-09 22:18:35 -0400

    Add files via upload

--- zsh-color-logging.plugin.zsh
+++ zsh-color-logging.plugin.zsh
@@ -1,4 +1,13 @@
-source ${0:A:h}/log.zsh
-source ${0:A:h}/config.conf
-source ${0:A:h}/helper.zsh
-source ${0:A:h}/.credentials.conf
+# Detect shell and source files accordingly
+if [ -n "$BASH_VERSION" ]; then
+    SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+elif [ -n "$ZSH_VERSION" ]; then
+    #SCRIPT_DIR=${0:A:h}
+    SCRIPT_DIR=$(dirname "${(%):-%N}")  # More robust for sourced scripts
+else
+    echo "Unknown shell. Configuration not sourced."
+fi
+source "${SCRIPT_DIR}/helper.zsh"
+source "${SCRIPT_DIR}/.credentials.conf"
+source "${SCRIPT_DIR}/config.conf"
+source "${SCRIPT_DIR}/log.zsh"
